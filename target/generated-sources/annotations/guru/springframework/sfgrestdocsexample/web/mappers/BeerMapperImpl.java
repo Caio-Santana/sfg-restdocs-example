32708b4af1f6db30f1198a00379c30c8ba0d261a
Caio-Santana <caio.santana@live.com>
2023-07-08 09:44:06 -0300

    Add Documenting validation Constraints

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-07T12:45:57-0300",
+    date = "2023-07-08T09:37:19-0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.2 (Oracle Corporation)"
 )
 @Component
